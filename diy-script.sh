c0db4b3e72eb790f75e927e51917c7340a5ee227
tonylee2022 <109421105+tonylee2022@users.noreply.github.com>
2025-05-01 13:40:56 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -6,6 +6,12 @@ sed -i 's/192\.168\.1\.1/192\.168\.5\.1/g' package/base-files/luci2/bin/config_g
 # 修改默认密码为空
 # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
+# 禁用版本信息修改
+sed -i '/^sed -i '\''\/DISTRIB_REVISION\/d'\'' \/etc\/openwrt_release$/s/^/#/' package/lean/default-settings/files/zzz-default-settings
+sed -i '/^echo "DISTRIB_REVISION='\''R25.4.145'\''" >> \/etc\/openwrt_release$/s/^/#/' package/lean/default-settings/files/zzz-default-settings
+sed -i '/^sed -i '\''\/DISTRIB_DESCRIPTION\/d'\'' \/etc\/openwrt_release$/s/^/#/' package/lean/default-settings/files/zzz-default-settings
+sed -i '/^echo "DISTRIB_DESCRIPTION='\''LEDE '\''" >> \/etc\/openwrt_release$/s/^/#/' package/lean/default-settings/files/zzz-default-settings
+
 # 更改默认 Shell 为 zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
@@ -87,9 +93,9 @@ sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/file
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
 # 修改版本为编译日期
-date_version=$(date +"%y.%m.%d")
-orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by TonyLee/g" package/lean/default-settings/files/zzz-default-settings
+# date_version=$(date +"%y.%m.%d")
+# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# sed -i "s/${orig_version}/R${date_version} by TonyLee/g" package/lean/default-settings/files/zzz-default-settings
 
 # 修复 hostapd 报错
 # cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
